a4b744d94abd1775dc575e35244eadf7a3a1c61b
Heber Mattos <heber.mattos@ilegra.com>
2020-10-06 00:29:48 -0300

    merge

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,8 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityModel;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
